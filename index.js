2680e3c255251394259fba5e8468ca1ae6457e32
iamsagarbha <32761309+iamsagarbha@users.noreply.github.com>
2018-04-19 10:36:00 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title> Sagarbha's Backbone Application </title>
+</head>
+<body>
+
+    <div id="app">
+        <div id="header">    </div>
+        <div id="app">    </div>
+        <div id="footer">    </div>
+    </div>
+</body>
+</html>
